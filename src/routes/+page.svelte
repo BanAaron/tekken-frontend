0671b2bda19a9249bab3e5fe8eb934706326d1ac
Khephren <aarontbarratt@gmail.com>
2023-11-18 21:06:27 +0000

    scuffed start

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Header from '$lib/Header.svelte';
+	import CharacterSelect from '$lib/CharacterSelect.svelte';
+</script>
+
+<Header />
+<div class="w-auto h-auto m-5">
+	<CharacterSelect />
+</div>
