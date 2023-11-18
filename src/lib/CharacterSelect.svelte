311875c4d7bdf255200bb3a800475f65a0456d86
Khephren <aarontbarratt@gmail.com>
2023-11-18 21:55:47 +0000

    drop down working

--- src/lib/CharacterSelect.svelte
+++ src/lib/CharacterSelect.svelte
@@ -1,7 +1,18 @@
-<script>
+<script lang="ts">
 	import { onMount } from 'svelte';
 
-	let characters;
+	type Character = {
+		Id: number;
+		ShortName: string;
+		LongName: string;
+		FightingStyle: string;
+		Nationality: string;
+		Height: number;
+		Weight: number;
+	};
+
+	let characters: Character[] = [];
+	$: characters = characters;
 
 	onMount(async () => {
 		try {
@@ -18,5 +29,8 @@
 	id="characters"
 	class="border border-neutral-800 bg-neutral-950 text-white focus:border-neutral-600 rounded-lg block w-full p-2.5"
 >
-	<option>Select a Character</option>
+	<option value="" disabled selected>Select a Character</option>
+	{#each characters as { Id, ShortName, LongName }}
+		<option id={Id.toString()} value={ShortName}>{LongName}</option>
+	{/each}
 </select>
