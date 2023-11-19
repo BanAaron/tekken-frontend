0c497e656a1773565ae01db66f4173984525d527
Khephren <aarontbarratt@gmail.com>
2023-11-19 20:28:30 +0000

    Railway backend (#2)
    * scuffed start
    
    * drop down working
    
    * using railroad api

--- src/lib/CharacterSelect.svelte
+++ src/lib/CharacterSelect.svelte
@@ -1,13 +1,24 @@
-<script>
-	import { onMount } from 'svelte';
+<script lang="ts">
+  import { onMount } from 'svelte';
+	import { api } from '$lib/vars';
 
-	let characters;
+	type Character = {
+		Id: number;
+		ShortName: string;
+		LongName: string;
+		FightingStyle: string;
+		Nationality: string;
+		Height: number;
+		Weight: number;
+	};
+
+	let characters: Character[] = [];
+	$: characters = characters;
 
 	onMount(async () => {
 		try {
-			const response = await fetch('http://localhost:8888/api/character');
+			const response = await fetch(api.character);
 			characters = await response.json();
-			console.log(characters);
 		} catch (error) {
 			console.error(error);
 		}
@@ -18,5 +29,8 @@
 	id="characters"
 	class="border border-neutral-800 bg-neutral-950 text-white focus:border-neutral-600 rounded-lg block w-full p-2.5"
 >
-	<option>Select a Character</option>
+	<option value="" disabled selected>Select a Character</option>
+	{#each characters as { Id, ShortName, LongName }}
+		<option id={Id.toString()} value={ShortName}>{LongName}</option>
+	{/each}
 </select>
---
+++ src/lib/vars.js
@@ -0,0 +1,5 @@
+export const api = {
+	root: 'https://tekken-backend-production.up.railway.app',
+	character: 'https://tekken-backend-production.up.railway.app/api/character',
+	teapot: 'https://tekken-backend-production.up.railway.app/api/teapot'
+};
